c8fed29ede2b3a2dece0b5d3e6a20c8271831380
chachamaru6 <kotaro.tech0130@gmail.com>
2021-08-27 16:15:23 +0900

    新規登録時保存できるようにした。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   describe "ユーザー新規登録" do
 
     it "nicknameが空だと登録できない" do
+     user = User.new(nickname: '', email: '')
     end
     it "emailが空では登録できない" do
     end
@@ -22,6 +23,6 @@ RSpec.describe User, type: :model do
     it "birthdayが空では登録できない" do
     end
   end
-  
+
 
 end
