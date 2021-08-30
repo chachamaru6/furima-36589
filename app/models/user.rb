ddbd96cbca535dafabf8d03c07ba354c360fc2fd
chachamaru6 <kotaro.tech0130@gmail.com>
2021-08-30 15:13:00 +0900

    テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  validates :last_name
+  validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カナを使用してください' } do
+  validates :last_name_kana
+  validates :first_name_kana
+  end
+
   validates :birthday, presence: true
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英数字混合にしてください' }
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+        nickname {"test"}
+        email {"test@gmail.com"}
+        password {Faker::Internet.password(min_length: 6)}
+        password_confirmation {password}
+        last_name {"ハローテスト"}
+        first_name {"バーイテスト"}
+        last_name_kana {"ハローテスト"}
+        first_name_kana {"バーイテスト"}
+        birthday {"2000-01-01"}
+      end
+    end
+
