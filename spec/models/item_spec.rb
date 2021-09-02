a335fe5a0f3721bdf148afb5361c0a65226d92a6
chachamaru6 <kotaro.tech0130@gmail.com>
2021-09-02 13:07:21 +0900

    userの情報の有無に対するテストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -85,7 +85,15 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price Out of setting range")
       end
 
+      it "userの情報がなければ出品できない" do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+
+    
      end
+
 
 
 
