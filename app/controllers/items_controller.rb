f66dda3685829091225ea8a8b1665690a7b275e5
chachamaru6 <kotaro.tech0130@gmail.com>
2021-09-03 14:19:58 +0900

    before_actionでまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :set_furima, only: [:show, :edit, :update]
   
   
 
@@ -22,23 +23,23 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     if @item.user_id != current_user.id
       redirect_to items_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
      if @item.update(item_params)
       redirect_to item_path
      else
       render :edit
      end
+     if @item.user_id != current_user.id
+      redirect_to items_path
+    end
   end
 
 
@@ -50,6 +51,10 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :status_id, :load_id, :prefecture_id, :schedule_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_furima
+    @item = Item.find(params[:id])
   end
 
 
