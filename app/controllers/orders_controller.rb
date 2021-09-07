842607b39b6e9f2a8e44f723de8a04e55b925b4f
chachamaru6 <kotaro.tech0130@gmail.com>
2021-09-07 16:08:03 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,7 +9,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-       pay_item
+       #pay_item
       @order_address.save
       redirect_to root_path 
     else
@@ -24,14 +24,14 @@ class OrdersController < ApplicationController
     params.require(:order_address).permit(:post_number, :prefecture_id, :city, :address_num, :building_name, :telephone,).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
-  def pay_item
-    Payjp.api_key = "sk_test_4d7982b796b399f42a463ee7"
-       Payjp::Charge.create(
-         amount: order_address_params[:price],  # 商品の値段
-         card: order_address_params[:token],    # カードトークン
-         currency: 'jpy'                 # 通貨の種類（日本円）
-       )
-  end
+  #def pay_item
+   # Payjp.api_key = "sk_test_4d7982b796b399f42a463ee7"
+    #   Payjp::Charge.create(
+     #    amount: order_address_params[:price],  # 商品の値段
+      #   card: order_address_params[:token],    # カードトークン
+        # currency: 'jpy'                 # 通貨の種類（日本円）
+       #)
+  #end
 
   
 end
