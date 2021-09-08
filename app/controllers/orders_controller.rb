ce697c74d8579a4d721bb69b9c577a14683e7c88
chachamaru6 <kotaro.tech0130@gmail.com>
2021-09-08 17:12:04 +0900

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,8 +30,8 @@ class OrdersController < ApplicationController
   def pay_item
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
-      amount: order_address_params[:price],  # 商品の値段
-      card: order_address_params[:token],    # カードトークン
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
   end
